d538a0d7258f6c41ec8a489635639bec545e5360
Yago Lizarribar <yago.lizarribar@gmail.com>
2023-10-30 09:54:54 +0100

    add rest interface example - non working version

--- src/TdoaRest.cc
+++ src/TdoaRest.cc
@@ -1,3 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
 //
-// Created by Yago Lizarribar on 30/10/23.
-//
+// Copyright (c) 2023 Yago Lizarribar
+
+#include <boost/program_options.hpp>
+#include <drogon/drogon.h>
+
+using namespace drogon;
+
+int main() {
+    app().registerHandler("/locate",
+                          [](const HttpRequestPtr &req,
+                             std::function<void(const HttpResponsePtr &)> &&callback,
+                             const std::string &name) {
+                              Json::Value json;
+                              json["result"] = "ok";
+                              json["message"] = std::string("hello,") + name;
+                              auto resp = HttpResponse::newHttpJsonResponse(json);
+                              callback(resp);
+                          },
+                          {Get, "LoginFilter"});
+
+    app().setLogPath("./")
+            .setLogLevel(trantor::Logger::kWarn)
+            .addListener("0.0.0.0", 80)
+            .setThreadNum(16)
+            .run();
+}
